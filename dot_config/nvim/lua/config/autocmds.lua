db7f52187203c82f3acd7b9a82e9c3e894b8f5de
hiroki.goto <blackjiro46248@gmail.com>
2023-04-23 21:41:53 +0900

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,4 +1,14 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
 vim.api.nvim_create_autocmd("BufWritePost", {
+  group = augroup("chezmoi_apply"),
   pattern = "~/.local/share/chezmoi/*",
   command = "chezmoi apply --source-path %",
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = augroup("cd_pwd"),
+  command = "cd %:p",
 })
