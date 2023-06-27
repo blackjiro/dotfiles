1e4489fe9d3e841840c624f0176b0f64f4c5e428
hiroki.goto <blackjiro46248@gmail.com>
2023-06-27 21:46:46 +0900

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,21 @@
-vim.keymap.set("n", "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", { desc = "Curret File History" })
-vim.keymap.set("n", "<leader>ge", "<cmd>DiffviewClose<cr>", { desc = "Exit diff view" })
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("n", "<S-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+map("n", "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", { desc = "Curret File History" })
+map("n", "<leader>ge", "<cmd>DiffviewClose<cr>", { desc = "Exit diff view" })
