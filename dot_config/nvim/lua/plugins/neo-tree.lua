b389655a877662261a33b4118338becb9c85f2ce
hiroki.goto <blackjiro46248@gmail.com>
2024-07-22 00:52:48 +0900

    update neo-tree

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,17 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
+    opts = function(_, opts)
+      opts.sources = { "filesystem", "buffers", "diagnostics" }
+      opts.filesystem = {
         filtered_items = {
           hide_dotfiles = false,
           hide_gitignored = false,
         },
-      },
-    },
+      }
+      opts.open_files_do_not_replace_types = opts.open_files_do_not_replace_types
+        or { "terminal", "Trouble", "qf", "Outline", "trouble" }
+      table.insert(opts.open_files_do_not_replace_types, "edgy")
+    end,
   },
 }
