37e38b191c60c4e5b1770744aaad3ad1cf3a4eac
hiroki.goto <blackjiro46248@gmail.com>
2024-07-25 11:18:17 +0900

    update color scheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,18 @@
 return {
-  -- { "doums/darcula" },
   { "rktjmp/lush.nvim" },
   { "briones-gabriel/darcula-solid.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "romainl/Apprentice" },
+  { "jacoborus/tender.vim" },
+  { "sainnhe/edge" },
+  { "folke/tokyonight.nvim" },
+  { "jeetsukumaran/vim-nefertiti" },
+  { "w0ng/vim-hybrid" },
+
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "darcula-solid",
+      colorscheme = "hybrid",
     },
   },
 }
