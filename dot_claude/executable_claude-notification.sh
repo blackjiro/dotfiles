3a91b0a5c7abf98e8855050ed8c6977d9ddfbe9a
hiroki.goto <blackjiro46248@gmail.com>
2025-07-02 21:30:12 +0900

    change sound

--- dot_claude/executable_claude-notification.sh
+++ dot_claude/executable_claude-notification.sh
@@ -18,4 +18,4 @@ repo=$(basename "$(git rev-parse --show-toplevel 2>/dev/null)" || echo "unknown"
 enhanced_message="$message [$repo ($branch)]"
 
 # Send notification using terminal-notifier
-terminal-notifier -title "$title" -message "$enhanced_message" -sound default
+terminal-notifier -title "$title" -message "$enhanced_message" -sound glass
