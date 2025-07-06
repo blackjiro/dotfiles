b786e98d0baaf3e937aa3e6ae25d131a20500fb5
Hiroki Goto <hirokigoto@HirokinoiMac.local>
2025-07-06 11:44:22 +0900

    fix markdown-preview issue

--- dot_config/nvim/lua/plugins/markdown-preview.lua
+++ dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -4,12 +4,13 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function()
+      vim.cmd([[Lazy load markdown-preview.nvim]])
       vim.fn["mkdp#util#install"]()
     end,
     config = function()
       -- Set custom CSS file path
       vim.g.mkdp_markdown_css = vim.fn.expand("~/.config/nvim/markdown-preview.css")
-      
+
       -- Other configuration options
       vim.g.mkdp_auto_start = 0 -- Don't auto-start preview
       vim.g.mkdp_auto_close = 1 -- Auto close when switching buffers
