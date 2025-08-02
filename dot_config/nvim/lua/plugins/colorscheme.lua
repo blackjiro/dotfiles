12c964b02145686fbbbdbc892a71ea117659c3aa
hiroki.goto <blackjiro46248@gmail.com>
2025-08-03 00:07:05 +0900

    update vim conf

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,17 @@
 return {
-  { "rktjmp/lush.nvim" },
   { "briones-gabriel/darcula-solid.nvim" },
   { "ellisonleao/gruvbox.nvim" },
-  { "romainl/Apprentice" },
-  { "jacoborus/tender.vim" },
   { "sainnhe/edge" },
   { "folke/tokyonight.nvim" },
-  { "jeetsukumaran/vim-nefertiti" },
-  { "w0ng/vim-hybrid" },
+  { "everviolet/nvim" },
+  { "sainnhe/sonokai" },
+  { "rebelot/kanagawa.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "sonokai",
     },
   },
 }
+
