21e2b575fbecb0ec56bc8ea424de162720b2e487
hiroki.goto <blackjiro46248@gmail.com>
2025-09-28 10:52:14 +0900

    Update Zellij tab name to show repository and branch

--- dot_config/fish/functions/zellij_update_tabname.fish
+++ dot_config/fish/functions/zellij_update_tabname.fish
@@ -1,11 +1,25 @@
 function zellij_update_tabname
     if set -q ZELLIJ
-        set current_dir $PWD
-        if test $current_dir = $HOME
-            set current_dir "~"
-        else
-            set current_dir (basename $current_dir)
+        set tab_name ""
+
+        set repo_name (git remote get-url origin 2>/dev/null | sed -E 's#.*/([^/]+)(\.git)?$#\1#' | sed 's/\.git$//')
+
+        if test -z "$repo_name"
+            set repo_name (basename (git rev-parse --show-toplevel 2>/dev/null))
         end
-        nohup zellij action rename-tab $current_dir >/dev/null 2>&1
+
+        set branch_name (git branch --show-current 2>/dev/null)
+
+        if test -n "$repo_name" -a -n "$branch_name"
+            set tab_name "$repo_name:$branch_name"
+        else
+            if test $PWD = $HOME
+                set tab_name "~"
+            else
+                set tab_name (basename $PWD)
+            end
+        end
+
+        nohup zellij action rename-tab $tab_name >/dev/null 2>&1
     end
 end
