f4443ce618d1fb5b34b40ed857bbd96d63cf7033
hiroki goto <blackjiro46248@gmail.com>
2026-01-20 17:06:51 +0900

    feat(nvim): replace markdown-preview.nvim with peek.nvim
    Mermaid対応のMarkdownプレビューのためpeek.nvimに移行

---
+++ dot_config/nvim/lua/plugins/peek.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup({
+        theme = "light",
+      })
+    end,
+    keys = {
+      {
+        "<leader>mp",
+        function()
+          local peek = require("peek")
+          if peek.is_open() then
+            peek.close()
+          else
+            peek.open()
+          end
+        end,
+        desc = "Peek Toggle",
+      },
+    },
+  },
+}
